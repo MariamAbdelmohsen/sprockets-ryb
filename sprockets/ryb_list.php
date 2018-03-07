051f9bdf7be8273aaed183a027c4af32eec40f00
thecbsnetwork <catherineblakesmith@gmail.com>
2018-03-07 10:38:18 -0800

    Creates pagination
    Creates pagination capabilities for Photos page. Adds Pager.php (created by Bill Newman) to includes folder, updates list page to include new functions. Uploads new images for navigation.

--- sprockets/ryb_list.php
+++ sprockets/ryb_list.php
@@ -1,20 +1,53 @@
-<?php include 'includes/config.php'?>
-<?php include 'includes/header.php'?>
+<?php 
+/**
+ * taken from demo_list_pager.php which demonstrates a list page that paginates data across 
+ * multiple pages
+ * 
+ * This page uses a Pager class which processes a mysqli SQL statement 
+ * and spans records across multiple pages. 
+ * 
+ * @package nmPager
+ * @author Bill Newman <williamnewman@gmail.com>
+ * @version 3.2 2015/11/24
+ * @link http://www.newmanix.com/
+ * @license http://www.apache.org/licenses/LICENSE-2.0 v. 3.0
+ * @see MyAutoLoader.php
+ * @see Pager.php 
+ * @todo none
+ */
+
+include 'includes/config.php';
+require 'includes/Pager.php'; #allows pagination
+# SQL statement
+$sql = "select * from ryb_album";
+
+#Fills <title> tag
+$title = 'Row Yr Boat/View/Pager';
+# END CONFIG AREA --------------------------------------------------------------
+
+include 'includes/header.php'?>
 <h1>Row Yr Boat</h1>
 <p><em>All photographs taken by <a href="https://www.facebook.com/DangerpantsPhotography/" target="_blank">Ian Johnston, Dangerpants Photography</a></em></p>
 <hr>
 
 <?php
-$sql = "select * from ryb_album";
-//we connect to the db here
-$iConn = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+$prev = '<img src="img/arrow_prev.gif" border="0" />';
+$next = '<img src="img/arrow_next.gif" border="0" />';
 
-//we extract the data here
-$result = mysqli_query($iConn,$sql);
+#Create a connection
+# connection comes first in mysqli (improved) function
+$iConn = @mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) or die(myerror(__FILE__,__LINE__,mysqli_connect_error()));
+
+#Create instance of new 'pager' class
+$myPager = new Pager(5,'',$prev,$next,'');
+$sql = $myPager->loadSQL($sql,$iConn); #load SQL, pass in existing connection, add offset
+$result = mysqli_query($iConn,$sql) or die(myerror(__FILE__,__LINE__,mysqli_error($iConn)));
 
 if(mysqli_num_rows($result) > 0)
-{//show records
-
+{#records exist - process
+    echo $myPager->showNAV();//show pager if enough records
+    if($myPager->showTotal()==1){$itemz = "photo";}else{$itemz = "photos";}//deal with plural
+    echo '<h4 align="center">There are ' . $myPager->showTotal() . ' ' . $itemz . '.</h4>';
     while($row = mysqli_fetch_assoc($result))
     {
         echo '<div><p>';
@@ -22,7 +55,8 @@ if(mysqli_num_rows($result) > 0)
         echo '' . $row['actors'] . '';
         echo '<blockquote>' . $row['quote'] . '</blockquote>';
         echo '</p></div><hr>';
-    }    
+    }  
+    echo $myPager->showNAV();//show pager if enough records
 
 }else{//inform there are no records
     echo '<p>There are no pictures.</p>';
